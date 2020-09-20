80f023f61f465465e40f34e929b1b4b3c663d8d3
felipe <felipe.siqueira@ccc.ufcg.edu.br>
2020-09-20 05:57:13 -0300

    corrigindo bug da entrada e traduzindo comentários para inglês

--- src/index.js
+++ src/index.js
@@ -1,66 +1,55 @@
-var fs = require('fs');
+let fs = require('fs');
+const prompt = require('prompt-sync')();
 
 
+//analyzes all files in a directory and returns the number of lines for each one
+function lineCounter(){
+	
+	const result = {};
 
-//analisa todos os arquivos em um diretorio e retorna a quantidade de linhas de cada um
-function contaLinhas(){
-	var readline = require('readline');
-	var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+	//asks for user input
+	const path = prompt('Enter the path to the directory to be scanned:     ');
 
-	});
+	//reads all files from a given directory
+	let arrayFiles = fs.readdirSync(path);
 
-	var retorno = new Map();
-
-	//Pedido de entrada
-	rl.question("Insira o caminho do dirétorio que deve ser analisado, Ex:/home/usuario/moenda/Moenda/doc\n", function(answer) {
+	//reading specific files and adding the number of lines to the 'result' variable.
+	for(let i = 0; i < arrayFiles.length; i++){
 		
-		//Lendo todos os arquivos existenstes na pasta informada pelo usuário
-		var arrayFiles = fs.readdirSync(answer)
-
-		//lendo arquivos específicos e adicionando a quantidade de linhas a variável 'retorno'.
-		for(var i = 0; i < arrayFiles.length; i++){
-			
-			const fileTest = fs.statSync(answer + "/" + arrayFiles[i], "utf-8");
-
-			if (fileTest.isFile()){
-				const file = fs.readFileSync(answer + "/" + arrayFiles[i], "utf-8");
-
-				retorno.set(arrayFiles[i], file.split("\n").length);
-
-			}
-		};
-
-		//retorna um map() com chave = nome do arquivo e valor = quantidade de linhas.        
-		console.log(retorno);
-
-		rl.close();
-
-	});
-}
-
-
-function coletaOitenta(link){
-	var retorno = [];
-				
-		const fileTest = fs.statSync(link, "utf-8");
+		const fileTest = fs.statSync(path + "/" + arrayFiles[i], "utf-8");
 
 		if (fileTest.isFile()){
-			const file = fs.readFileSync(link, "utf-8");
+			const file = fs.readFileSync(path + "/" + arrayFiles[i], "utf-8");
+			result[`${arrayFiles[i]}`] = file.split("\n").length - 1;
+		}	
+	};
+	
+	return result;
+}
 
-			const lines = file.split("\n");
 
-			for(var i = 0; i<lines.length; i++){
-				if (lines[i].trim().length > 80){
-					retorno.push([i]+ " : " + lines[i]);
-				}
+//analyzes a given file and stores lines that have more than 80 characters
+function collectEighty(link){
+	
+	let result = [];
+
+	//tests whether the past path leads to a directory or file
+	const fileTest = fs.statSync(link, "utf-8");
+
+	if (fileTest.isFile()){
+		const file = fs.readFileSync(link, "utf-8");
+
+		const lines = file.split("\n");
+
+		for(let i = 0; i<lines.length; i++){
+			if (lines[i].trim().length > 80){
+				result.push([i]+ " : " + lines[i]);
 			}
-			
 		}
+	}
 
-	//retorna um map() com chave = número da linha e valor = linha.
-	return retorno;
+	//returns a object() with key = line number and value = line.
+	return result;
 
 }
-contaLinhas();
+
