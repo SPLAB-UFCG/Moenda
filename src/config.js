03bcd794938a3ea916f7c78616705680bbf0b7e7
felipe <felipe.siqueira@ccc.ufcg.edu.br>
2020-11-04 01:59:09 -0300

    Adicionando novas funcionalidades, executando regras de arquivo externo, criando arquivo de configurações.

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 const config = {
   lineCounter: {limit:60},
   hasLineAboveXCharacters: {limit:30},
-  
-  
+  firstSectionStartsWithHx: {limit: 3},
+  consecutiveBlankLines: {limit: 1}
 }
 
 module.exports = config;
